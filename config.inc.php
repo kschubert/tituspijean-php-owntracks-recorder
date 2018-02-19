bae0ae392417ed011062771b8ac3fda2f37aade7
tituspijean <tituspijean@outlook.com>
2018-02-19 22:47:39 +0100

    Add config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+	$_config['sql_host'] = 'locahost';
+	$_config['sql_user'] = '__USER__';
+	$_config['sql_pass'] = '__DB_PWD__';
+	$_config['sql_db'] = '__USER__';
+
+	$_config['sql_prefix'] = '';
+
+	$_config['default_accuracy'] = 1000;
+
+?>
